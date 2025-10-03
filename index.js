9046b9fb89185b43eb61ef58bec0aafaaaa55937
Valerie Racine <116108359+v-racine@users.noreply.github.com>
2025-10-03 15:41:48 -0700

    feat: decoding (#3)
    * feat: decoding
    
    * cleanup

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
   const messageDiv = document.querySelector("#message");
   const linkDiv = document.querySelector("#link");
+  const showDiv = document.querySelector("#message-show");
   const message = document.querySelector("#message-input");
   const link = document.querySelector("#link-input");
+  const h1 = document.querySelector("#message-show h1");
+
+  const { hash } = window.location;
+  const messageHash = hash.replace("#", "");
+
+  if (messageHash) {
+    messageDiv.classList.add("hide");
+    showDiv.classList.remove("hide");
+    try {
+      const decoded = atob(messageHash);
+      const secretMessage = decodeURIComponent(escape(decoded));
+      h1.textContent = secretMessage;
+    } catch (err) {
+      h1.textContent = "Invalid secret message";
+      console.error(err);
+    }
+  }
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
