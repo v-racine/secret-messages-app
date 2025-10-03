d260633c4c864b9bd26b771b76056080c674d064
Valerie Racine <vracine21@gmail.com>
2025-10-03 15:27:51 -0700

    feat: decoding

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
   const messageDiv = document.querySelector("#message");
   const linkDiv = document.querySelector("#link");
+  const showDiv = document.querySelector("#message-show");
   const message = document.querySelector("#message-input");
   const link = document.querySelector("#link-input");
+  const h1 = document.querySelector("h1");
+
+  const { hash } = window.location;
+  const secretMessage = atob(hash.replace("#", ""));
+
+  if (secretMessage) {
+    messageDiv.classList.add("hide");
+    showDiv.classList.remove("hide");
+    h1.textContent = secretMessage;
+  }
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
